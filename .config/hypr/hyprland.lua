d04eddcaee695629816f8c8ddc8d720a572282d0
aseer-neufast <aseer@mail.neufast.com>
2026-08-22 21:52:53 +0600

    update waypaper setting

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -16,7 +16,7 @@ hl.monitor({
 hl.on("hyprland.start", function()
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
 	hl.exec_cmd("waybar")
-	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("waypaper --restore")
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user start hyprland-session.target")
 	hl.exec_cmd("swaync")
